987d3821c46116c984102e974a9f1e8f388067b2
Ross Keenan <rkeenan@auroracoop.com>
2020-10-28 16:48:17 -0500

    Adjusted post.js controller, so the default value for likes is 0 instead of null.

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,23 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            /* This sets the default of likes to 0, so no one can update it to a certain number to game the system.*/
+            entity = await strapi.services.post.create({...data, likes: 0}, {files});
+        } else {
+            entity = await strapi.services.post.create({...ctx.request.body, likes: 0});
+        }
+        return sanitizeEntity(entity, { model: strapi.models.post });
+        },
+    };  
+
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
