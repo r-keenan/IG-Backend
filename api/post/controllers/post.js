e5c6d1b73bbe2e6be0e04cc6b75632e0e44121d0
Ross Keenan <rkeenan@auroracoop.com>
2020-10-29 14:52:49 -0500

    Add backend validation and custom error messages for backend.

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -6,13 +6,22 @@ module.exports = {
     async create(ctx) {
         let entity;
         if (ctx.is('multipart')) {
-            const { data, files } = parseMultipartData(ctx);
+            const { data, files } = parseMultipartData(ctx)
+
+            if(!data || !data.description){
+                ctx.throw(400, "Please write a description")
+            }
+            if(!files || !files.image){
+                ctx.throw(400, "Please upload an Image")
+            }
             /* This sets the default of likes to 0, so no one can update it to a certain number to game the system.*/
-            entity = await strapi.services.post.create({...data, likes: 0}, {files});
+            entity = await strapi.services.post.create({...data, likes: 0}, {files})
         } else {
-            entity = await strapi.services.post.create({...ctx.request.body, likes: 0});
+            /*eliminating the line below and adding other line makes it so you have to submit a picture(validation)*/
+            /*entity = await strapi.services.post.create({...ctx.request.body, likes: 0});*/
+            ctx.throw(400, "Please use multipart/form-data")
         }
-        return sanitizeEntity(entity, { model: strapi.models.post });
+            return sanitizeEntity(entity, { model: strapi.models.post })
         },
     };  
 
